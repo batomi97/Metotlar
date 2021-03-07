24f7768aed317058006687ee5a5df49fb90720e5
Batuhan <batomi97@hotmail.com>
2021-03-07 17:10:41 +0300

    7 Mart Projesi,

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //encapsulation
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urun.Adi);
+        }
+
+        //Kullanılmaması gereken bir yöntem. Hatalı yöntem
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
